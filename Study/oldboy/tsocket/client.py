71660af4e1f34a004ace7004c382b7f89f333f95
leal <lijiadongyue@gmail.com>
2018-06-12 22:36:38 +0800

    format class

--- Study/oldboy/tsocket/client.py
+++ Study/oldboy/tsocket/client.py
@@ -7,88 +7,89 @@ import socket
 import os
 
 
-def test1(port, ip="127.0.0.1"):
-    sk = socket.socket()
-    address = (ip, port)
-    try:
+class TestSocketClient(object):
+    def __init__(self, port, ip="127.0.0.1"):
+        self.port = port
+        self.ip = ip
+
+    def get_connect(self):
+        # 1.创建对象
+        sk = socket.socket()
+        # 2.连接服务端指定的ip地址和端口
+        address = (self.ip, self.port)
         sk.connect(address)
         print("SK:%s" % sk)
-        count = 0
-        while count < 5:
-            cin = input(">>>>>")
-            if cin.casefold() == "exit":
+        return sk, address
+
+    def test1(self):
+
+        try:
+            sk, address = self.get_connect()
+            count = 0
+            while count < 5:
+                cin = input(">>>>>")
+                if cin.casefold() == "exit":
+                    break
+                else:
+                    sk.send(bytes(cin, "utf8"))
+                    data = sk.recv(1024)
+                    print("%s:%s" % (str(address[1]), str(data, "utf8")))
+                count = count + 1
+            sk.close()
+        except ConnectionAbortedError as error:
+            print("Server is offline", error)
+            return 0
+
+    def test_cmd(self):
+        """
+        subprocess.PIPE把输出放进管道PIPE
+        :param command:
+        :return:
+        """
+        sk, address = self.get_connect()
+        while True:
+            command = input(">>>>")
+            if command.casefold() == "exit":
                 break
-            else:
-                sk.send(bytes(cin, "utf8"))
-                data = sk.recv(1024)
-                print("%s:%s" % (str(address[1]), str(data, "utf8")))
-            count = count + 1
+            # 3.发送命令
+            sk.send(bytes(command, "utf8"))
+            # 4.获取返回结果并打印
+            # 解决结果大于1024的接收问题，先接收数据的长度，然后累加
+            receive_length = int(str(sk.recv(1024), "utf8"))
+            print("Length:", receive_length)
+            receive_data = bytes()
+            while len(receive_data) != receive_length:
+                print(len(receive_data))
+                receive_data += sk.recv(1024)
+                print(len(receive_data))
+            print("Execute %s Result is:%s" % (command, str(receive_data, "utf8")))
         sk.close()
-    except ConnectionAbortedError as error:
-        print("Server is offline", error)
-        return 0
 
-
-def test_cmd(port, ip="127.0.0.1"):
-    """
-    subprocess.PIPE把输出放进管道PIPE
-    :param command:
-    :return:
-    """
-    # 1.创建对象
-    sk = socket.socket()
-    # 2.连接服务端指定的ip地址和端口
-    address = (ip, port)
-    sk.connect(address)
-    while True:
-        command = input(">>>>")
-        if command.casefold() == "exit":
-            break
-        # 3.发送命令
-        sk.send(bytes(command, "utf8"))
-        # 4.获取返回结果并打印
-        # 解决结果大于1024的接收问题，先接收数据的长度，然后累加
-        receive_length = int(str(sk.recv(1024), "utf8"))
-        print("Length:", receive_length)
-        receive_data = bytes()
-        while len(receive_data) != receive_length:
-            print(len(receive_data))
-            receive_data += sk.recv(1024)
-            print(len(receive_data))
-        print("Execute %s Result is:%s" % (command, str(receive_data, "utf8")))
-    sk.close()
-
-
-def test_image(port, ip="127.0.0.1"):
-    # 1.创建对象
-    sk = socket.socket()
-    # 2.连接服务端指定的ip地址和端口
-    address = (ip, port)
-    sk.connect(address)
-    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-    while True:
-        command = input(">>>>").strip()
-        # command = "Post C:/Users/leali/Pictures/Camera Roll/Tim.png"
-        cmd, path = command.split(" ")
-        # 1 获取文件信息
-        path = os.path.join(BASE_PATH, path)
-        filename = os.path.basename(path)
-        # os.stat(path).st_size
-        file_size = os.path.getsize(filename)
-        # 2.发送文件信息
-        file_info = "post %s %s" % (filename, file_size)
-        sk.send(file_info.encode("utf8"))
-        # 发送文件
-        with open(path, "rb") as file:
-            has_sent = 0
-            while has_sent != file_size:
-                send_data = file.read(1024)
-                sk.sendall(send_data)
-                has_sent += len(send_data)
-            print("Send Successfully")
+    def test_image(self):
+        sk, address = self.get_connect()
+        BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+        while True:
+            command = input(">>>>").strip()
+            # command = "Post C:/Users/leali/Pictures/Camera Roll/Tim.png"
+            cmd, path = command.split(" ")
+            # 1 获取文件信息
+            path = os.path.join(BASE_PATH, path)
+            filename = os.path.basename(path)
+            # os.stat(path).st_size
+            file_size = os.path.getsize(filename)
+            # 2.发送文件信息
+            file_info = "post %s %s" % (filename, file_size)
+            sk.send(file_info.encode("utf8"))
+            # 发送文件
+            with open(path, "rb") as file:
+                has_sent = 0
+                while has_sent != file_size:
+                    send_data = file.read(1024)
+                    sk.sendall(send_data)
+                    has_sent += len(send_data)
+                print("Send Successfully")
 
 
 if __name__ == "__main__":
-    # test1(1226)
-    # test_cmd(1226)
-    test_image(1226)
+    test_socket_client = TestSocketClient(1226)
+    test_socket_client.test_image()
--- Study/oldboy/tsocket/server.py
+++ Study/oldboy/tsocket/server.py
@@ -6,115 +6,106 @@
 import socket
 import subprocess
 import os
+import socketserver
 
 
-def test1(port, ip="127.0.0.1"):
-    """
-    记住server与client的区别，server通过绑定ip，通过conn交互
-    client通过socket连接交互
-    :param port:
-    :param ip:
-    :return:
-    """
-    sk = socket.socket()
-    address = (ip, port)
-    sk.bind(address)
-    print("Server online ,Waiting connect……")
-    sk.listen(3)  # waiting connect numbers
-    count = 0
-    while True:
+class TestSocketServer(object):
+
+    def __init__(self, port, ip="127.0.0.1"):
+        self.port = port
+        self.ip = ip
+
+    def bind_server(self):
+        # 1.实例化套接字对象
+        sk = socket.socket()
+        # 2.指定连接的ip地址和端口
+        address = (self.ip, self.port)
+        # 3 服务器绑定
+        sk.bind(address)
+        # 4 设置连接等待数
+        sk.listen(3)
+        # waiting connect,get accept
+        print("%s is online,waiting connect" % address[1])
+        # 5.准备监听，获取连接信息
         conn, addr = sk.accept()
-        print("SK ACCEPT:%s" % str(conn))
-        while count < 3:
-            try:
-                data = conn.recv(1024)  # data is not null
+        print("%s Connected successfully" % addr[1])
+        return sk, conn, addr
 
-                if not data:
+    def test1(self):
+        """
+        记住server与client的区别，server通过绑定ip，通过conn交互
+        client通过socket连接交互
+        :param port:
+        :return:
+        """
+        sk, conn, addr = self.bind_server()
+        count = 0
+        while True:
+            conn, addr = sk.accept()
+            print("SK ACCEPT:%s" % str(conn))
+            while count < 3:
+                try:
+                    data = conn.recv(1024)  # data is not null
+                    if not data:
+                        break
+                    else:
+                        print("%s:%s" % (addr[1], str(data, "utf8")))
+                        # 回信
+                        cin = input(">>>>>")
+                        # 1.ConnectionResetError 如果没有发送消息直接force disconnect,在recv()除捕获
+                        # 如果先发送信息，后捕获，位置于send()方法
+                        # 2.在Linux 环境下，默认强制断开为发送空消息，不会跑出异常
+                        conn.send(bytes(cin, "utf8"))
+                        count = count + 1
+                except ConnectionResetError as error:
+                    print("Client force disconnect:", error)
                     break
-                else:
-                    print("%s:%s" % (addr[1], str(data, "utf8")))
-                    # 回信
-                    cin = input(">>>>>")
-                    # 1.ConnectionResetError 如果没有发送消息直接force disconnect,在recv()除捕获
-                    # 如果先发送信息，后捕获，位置于send()方法
-                    # 2.在Linux 环境下，默认强制断开为发送空消息，不会跑出异常
-                    conn.send(bytes(cin, "utf8"))
-                    count = count + 1
-            except ConnectionResetError as error:
-                print("Client force disconnect:", error)
+            conn.close()
+        sk.close()
+
+    def test_cmd(self):
+        sk, conn, addr = self.bind_server()
+        # 6.准备接收客户端命令，执行返回结果
+        while True:
+            command = conn.recv(1024)
+            if not command:
                 break
+            result = subprocess.Popen(str(command, "utf8"), shell=True, stdout=subprocess.PIPE)
+            # 坑result.stdout.read()只能读取一次，第二次就会为空，因此不要尝试使用进行二次赋值
+            result_data = result.stdout.read()
+            result_length = bytes(str(len(result_data)), "utf8")
+            # sendall会把数据直接全部发送到客户端，客户端将所有的数据都放到缓冲区,
+            # 每次recv多少字节取决于recv内的参数，理论不应该超过8k
+            conn.sendall(result_length)
+            conn.sendall(result_data)
         conn.close()
-    sk.close()
+        sk.close()
 
+    def test_image(self):
+        sk, conn, addr = self.bind_server()
+        # 6.准备接收客户端命令，执行返回结果
+        BASE_PATH = os.path.abspath(os.path.join(os.path.dirname("__file__"), os.path.pardir))
+        print("BASE_PATH:", BASE_PATH)
+        while True:
+            # 接收文件信息
+            file_info = conn.recv(1024)
+            cmd, filename, file_size = file_info.decode("utf8").split(" ")
+            # 构造路径
+            path = os.path.join(BASE_PATH, "data", filename)
+            print("Path:", path)
+            file_size = int(file_size)
+            # 接收文件
+            with open(path, "ab") as f:
+                has_received = 0
+                while has_received != file_size:
+                    receive_data = conn.recv(1024)
+                    f.write(receive_data)
+                    has_received += len(receive_data)
+                print("Receive Successfully")
 
-def test_cmd(port, ip="127.0.0.1"):
-    # 1.实例化套接字对象
-    sk = socket.socket()
-    # 2.指定连接的ip地址和端口
-    address = (ip, port)
-    # 3 服务器绑定
-    sk.bind(address)
-    # 4 设置连接等待数
-    sk.listen(3)
-    # waiting connect,get accept
-    print("%s is online,waiting connect" % address[1])
-    # 5.准备监听，获取连接信息
-    conn, addr = sk.accept()
-    print("%s Connected successfully" % addr[1])
-    # 6.准备接收客户端命令，执行返回结果
-    while True:
-        command = conn.recv(1024)
-        if not command:
-            break
-        result = subprocess.Popen(str(command, "utf8"), shell=True, stdout=subprocess.PIPE)
-        # 坑result.stdout.read()只能读取一次，第二次就会为空，因此不要尝试使用进行二次赋值
-        result_data = result.stdout.read()
-        result_length = bytes(str(len(result_data)), "utf8")
-        # sendall会把数据直接全部发送到客户端，客户端将所有的数据都放到缓冲区,
-        # 每次recv多少字节取决于recv内的参数，理论不应该超过8k
-        conn.sendall(result_length)
-        conn.sendall(result_data)
-    conn.close()
-    sk.close()
-
-
-def test_image(port, ip="127.0.0.1"):
-    # 1.实例化套接字对象
-    sk = socket.socket()
-    # 2.指定连接的ip地址和端口
-    address = (ip, port)
-    # 3 服务器绑定
-    sk.bind(address)
-    # 4 设置连接等待数
-    sk.listen(3)
-    # waiting connect,get accept
-    print("%s is online,waiting connect" % address[1])
-    # 5.准备监听，获取连接信息
-    conn, addr = sk.accept()
-    print("%s Connected successfully" % addr[1])
-    # 6.准备接收客户端命令，执行返回结果
-    BASE_PATH = os.path.abspath(os.path.join(os.path.dirname("__file__"), os.path.pardir))
-    print("BASE_PATH:", BASE_PATH)
-    while True:
-        # 接收文件信息
-        file_info = conn.recv(1024)
-        cmd, filename, file_size = file_info.decode("utf8").split(" ")
-        # 构造路径
-        path = os.path.join(BASE_PATH, "data", filename)
-        print("Path:", path)
-        file_size = int(file_size)
-        # 接收文件
-        with open(path, "ab") as f:
-            has_received = 0
-            while has_received != file_size:
-                receive_data = conn.recv(1024)
-                f.write(receive_data)
-                has_received += len(receive_data)
-            print("Receive Successfully")
-
-    conn.close
+        conn.close
 
 
 if __name__ == "__main__":
-    # test_cmd(1226)
-    test_image(1226)
+    test_socket = TestSocketServer(1226)
+    test_socket.test_image()
