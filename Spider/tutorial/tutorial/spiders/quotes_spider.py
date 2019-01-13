782423f7ab4422a903e6664abcccc54fd584a2c5
leal <lijiadongyue@gmail.com>
2019-01-13 17:37:36 +0800

    scrapy tutorial

---
+++ Spider/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Author:leali
+# Description:
+# Version:v1.0
+# Date:2019-01-01 04:38 PM
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
